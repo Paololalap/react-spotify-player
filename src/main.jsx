7d6998caf07629aab57067c59b6387f8e6f49922
paololalap <paolomorales1502@gmail.com>
2024-03-08 23:37:49 +0800

    Initial Commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './reset.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { CustomProvider } from "rsuite";
+import App from "./App.jsx";
+import "./reset.css";
+import "rsuite/dist/rsuite.min.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <CustomProvider theme='dark'>
+      <App />
+    </CustomProvider>
+  </React.StrictMode>
+);
+
