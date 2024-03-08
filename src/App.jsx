9347bb1e503b142031519332faf71296d99dca5d
paololalap <paolomorales1502@gmail.com>
2024-03-08 13:44:48 +0800

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PlayerPage from './Pages/PlayerPage';
+
+const App = () => {
+  return (
+    <>
+      <PlayerPage></PlayerPage>
+    </>
+  )
+}
+
+export default App;
